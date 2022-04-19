04a877cb9e40c254cd752d081176792bb28028bc
Brian7950 <brianh7950@gmail.com>
2022-04-18 22:58:38 -0600

    set up api get/post & get for html

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const uuid = require('uuid');
 const fs = require('fs');
 const notes = require('./db/db.json');
 
@@ -7,14 +8,44 @@ const notes = require('./db/db.json');
 const app = express();
 
 //what port to listen to for responses
-const port = 3001;
+const Port = 3001;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+//middleware
+app.use(express.urlencoded(({ extended: true})));
+app.use(express.json());
+
+//static middleware (allows files that are dependant on one another be ran)
+app.use(express.static('./public'));
+
+//GET API db.json
+app.get('api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './db/db.json'));
+})
+
+//function to add notes to db.json via POST
+app.post('api/notes', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('./db/db.json'));
+    const newNotes = req.body;
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes))
+    res.json(notes);
+});
+
+
+//Load HTML homepage
+app.get('/',(req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+//load notes.html file
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+//listen to port
+app.listen(Port, () => {
+    console.log(`I'm listening to you on port ${Port}!`);
+});
 
 
